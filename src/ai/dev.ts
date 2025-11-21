9d9045ff3c5d85ada234ce2a4717f2f92e7c1129
Anabella Lujan Medrano <analujan761@gmail.com>
2025-11-21 13:10:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/ai/dev.ts
+++
@@ -1,5 +0,0 @@
-'use server';
-// Flows will be imported for their side effects in this file.
-import './flows/diagnose-plant-flow';
-import './flows/vegetable-recommender-flow';
-import './flows/identify-plant-flow';
