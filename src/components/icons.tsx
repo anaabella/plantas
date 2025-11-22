eda833df53f3efe0695e0225db052b40cc5e5e28
Anabella Lujan Medrano <analujan761@gmail.com>
2025-11-22 00:37:05 +0000

    u

--- src/components/icons.tsx
+++
@@ -1,8 +0,0 @@
-import { Sprout } from 'lucide-react';
-import type { SVGProps } from 'react';
-
-export function Logo(props: SVGProps<SVGSVGElement>) {
-  return (
-    <Sprout className="h-6 w-6 text-primary" {...props} />
-  );
-}
